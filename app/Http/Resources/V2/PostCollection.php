4765f51eeb559058589259ddded0fea1be9e1f27
Eduardo ARG <eduardo1590@gmail.com>
2021-06-03 08:35:38 -0400

    index y show v2

--- app/Http/Resources/V2/PostCollection.php
+++ app/Http/Resources/V2/PostCollection.php
@@ -14,6 +14,17 @@ class PostCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'data' => $this->collection,
+            'meta' => [
+                'organization' => 'Platzi',
+                'authors' => [
+                    'Italo Morales',
+                    'Platzi',
+                    'Eduardo Rahn'
+                ]
+                ],
+                'type' => 'articles'
+        ];
     }
 }

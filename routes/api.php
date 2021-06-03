d1b1184692e477918d6900f7a71148594fe972c0
Eduardo ARG <eduardo1590@gmail.com>
2021-06-03 18:30:16 -0400

    ruta login y proteccion de rutas

--- routes/api.php
+++ routes/api.php
@@ -22,4 +22,7 @@ use App\Http\Controllers\Api\V2\PostController as PostV2;
 
 Route::apiResource('v1/posts', PostV1::class)->only(['index','show', 'destroy']);
 
-Route::apiResource('v2/posts', PostV2::class)->only(['index','show', 'destroy']);
+Route::apiResource('v2/posts', PostV2::class)->only(['index','show'])
+    ->middleware('auth:sanctum');
+
+Route::post('login', [App\Http\Controllers\Api\LoginController::class, 'login']);

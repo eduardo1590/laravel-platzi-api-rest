f7d0c4558e56e49f462b27fc96e2ad9f1cb3f2f8
Eduardo ARG <eduardo1590@gmail.com>
2021-06-03 08:08:57 -0400

    ruta v2

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\PostController as PostV1;
+use App\Http\Controllers\Api\V2\PostController as PostV2;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,4 +20,6 @@ use Illuminate\Support\Facades\Route;
     return $request->user();
 });*/
 
-Route::apiResource('v1/posts', App\Http\Controllers\Api\V1\PostController::class)->only(['index','show', 'destroy']);
+Route::apiResource('v1/posts', PostV1::class)->only(['index','show', 'destroy']);
+
+Route::apiResource('v2/posts', PostV2::class)->only(['index','show', 'destroy']);
